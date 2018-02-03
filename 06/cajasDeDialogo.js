2b6acdc866885328c0836c50126d13920801887c
megacep <35774142+megacep@users.noreply.github.com>
2018-02-03 17:26:57 -0300

    notificaciones o cajas de dialogo

---
+++ 06/cajasDeDialogo.js
@@ -0,0 +1,16 @@
+console.log("********** cajas de dialogo ************");
+
+// por algun motivo recomienda utilizar libreria que implementen
+// cajas de dialogs y no asi, porque estas se puede bloquear e 
+// hirce todo la jocaren
+// sweet alert (buscar notification javascript)
+
+
+alert("ppuua");
+
+var acepto = confirm("Esta seguro de desea borrarr...");
+console.log(acepto);
+
+
+var pepe = prompt("ingrese su nombre","nombre");
+console.log(pepe);
